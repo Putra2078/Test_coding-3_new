d5b8feebdea1f9c41fc7892b6e46dd99a9e20370
Putra2078 <141045021+Putra2078@users.noreply.github.com>
2023-07-31 21:30:31 +0700

    Add files via upload

---
+++ Test_Code-3.py
@@ -0,0 +1,8 @@
+celcius = float(input("Tuliskan suhu dalam Celcius:30"))
+
+fahrenheit = (celcius * 1.8) + 32
+
+print('0.2f Derajat Celcius Sama Derajat Fahrenheit' %(celcius,fahrenheit))
+
+Tuliskan Suhu dalam Celcius: 30
+30.00 Derajat Celcius sama dengan 86.00 Derajat Fahrenheit
